ddfd50c1f3a2b8ebbf5c84033e12dbc4dae0358b
Anton Podviaznikov <podviaznikov@gmail.com>
2013-06-02 20:45:39 -0600

    initial commit

---
+++ src/blog/hashobject/com.clj
@@ -0,0 +1,6 @@
+(ns blog.hashobject.com)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/blog/hashobject/com_test.clj
@@ -0,0 +1,7 @@
+(ns blog.hashobject.com-test
+  (:use clojure.test
+        blog.hashobject.com))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
