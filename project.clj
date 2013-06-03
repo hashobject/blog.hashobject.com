3f6d3cbde3ef109f3108feab9b4b76d97e1f82eb
Anton Podviaznikov <podviaznikov@gmail.com>
2013-06-02 23:05:48 -0600

    start work on html generator from markdown post

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "HashObject team technical blog. Generated using Clojure. Hosted on Amazon S3."
   :url "http://blog.hashobject.com"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [hiccup "1.0.3"]]
+                 [hiccup "1.0.3"]
+                 [markdown-clj "0.9.26"]]
   :plugins [[lein-shell "0.1.0"]]
   :aliases {"html" ["trampoline" "run" "-m" "blog.hashobject.generator/generate"]
             "site-build" ["shell" "./frontend/node_modules/grunt-cli/bin/grunt" "--gruntfile" "frontend/Gruntfile.js" "build"]
--- src/blog/hashobject/generator.clj
+++ src/blog/hashobject/generator.clj
@@ -1,9 +1,20 @@
 (ns blog.hashobject.generator
   (:use clojure.java.io)
-  (:require [blog.hashobject.views.index :as index-view]))
+  (:require [markdown.core :as markdown]
+            [blog.hashobject.views.index :as index-view]))
+
+(def sources-dir (clojure.java.io/file "./resources/posts/"))
+
+(def posts-files
+    (for [file (file-seq sources-dir)] (.getName file)))
+
+(defn get-files-to-process []
+    (into [] (filter #(.endsWith % ".md") posts-files)))
+
 
 
 (defn generate []
   (spit (str "./resources/public/index.html") (index-view/index)))
 
 
+(get-files-to-process)
