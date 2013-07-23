2c6fddcf911476cc3e3e3e35b056fcc3f6ddeb5e
Anton Podviaznikov <podviaznikov@gmail.com>
2013-07-22 23:49:12 -0600

    new blog post

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -54,6 +54,11 @@ module.exports = function(grunt) {
           gzip: true
         },
         {
+          src: '../resources/public/website-launch-checklist/index.html',
+          dest: 'website-launch-checklist/index.html',
+          gzip: true
+        },
+        {
           src: '../resources/public/css/app.css',
           dest: 'css/app.css',
           gzip: true
