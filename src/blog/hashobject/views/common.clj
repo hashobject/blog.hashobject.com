98c782e1be88c5f17633e061bcc711397b9e7950
Anton Podviaznikov <podviaznikov@gmail.com>
2013-12-25 17:49:39 -0500

    working on topbar

--- src/blog/hashobject/views/common.clj
+++ src/blog/hashobject/views/common.clj
@@ -14,14 +14,19 @@
 
 
 (defn header []
-  [:header.row
-   [:div.small-12
-    [:a {:href "http://blog.hashobject.com"}[:div.logo]]
-    [:nav {:role "navigation"}
-     [:ul
-      [:li [:a {:href "http://hashobject.com"} "Home"]]
-      [:li.active [:a {:href "http://blog.hashobject.com"} "Blog"]]
-      [:li [:a {:href "http://os.hashobject.com"} "Open Source"]]]]]])
+  [:header.contain-to-grid
+    [:nav.top-bar {:role "navigation" :data-topbar ""}
+     [:ul.title-area
+       [:li.name
+        [:h1.hide-for-small-only
+         [:a {:href "http://blog.hashobject.com"}[:div.logo]]]]
+       [:li.toggle-topbar.menu-icon
+        [:a {:href "#"} [:div.logo.square]]]]
+     [:section.top-bar-section
+       [:ul.right
+        [:li [:a {:href "http://hashobject.com"} "Home"]]
+        [:li.active [:a {:href "http://blog.hashobject.com"} "Blog"]]
+        [:li [:a {:href "http://os.hashobject.com"} "Open Source"]]]]]])
 
 (defn footer []
   [:footer.row
@@ -29,4 +34,8 @@
     [:p.licence "Except as otherwise noted, the content of this site is licensed
      under the <a href='http://creativecommons.org/licenses/by/3.0/'>Creative Commons Attribution 3.0 License</a>,
      and code samples are licensed under the
-     <a href='http://opensource.org/licenses/eclipse-1.0'>Eclipse Public License 1.0</a>."]]])
+     <a href='http://opensource.org/licenses/eclipse-1.0'>Eclipse Public License 1.0</a>."]]
+   (include-js "/js/jquery.js")
+   (include-js "/js/foundation.min.js")
+   (include-js "/js/foundation.topbar.js")
+   [:script "$(document).foundation();"]])
