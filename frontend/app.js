2d49224ad3987aa2dc29c5a1075bdee9cf9d984b
Anton Podviaznikov <podviaznikov@gmail.com>
2014-12-23 22:09:55 -0800

    move to gulp

--- frontend/app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-
-app.use(express.static('../resources/public'));
-
-app.listen(3000);
-console.log('Listening on port 3000');
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+var gulp = require('gulp');
+var path = require('path');
+var fs   = require('fs');
+var awspublish = require('gulp-awspublish');
+
+var aws = JSON.parse(fs.readFileSync('./aws.json'));
+
+
+
+
+gulp.task('deploy', function() {
+
+  // create a new publisher
+  var publisher = awspublish.create(aws);
+
+  // define custom headers
+  var headers = {
+     'Cache-Control': 'max-age=315360000, no-transform, public'
+  };
+
+  return gulp.src('./resources/public/**/*.*')
+
+     // gzip, Set Content-Encoding headers and add .gz extension
+   // .pipe(awspublish.gzip({ ext: '.gz' }))
+
+    // publisher will add Content-Length, Content-Type and  headers specified above
+    // If not specified it will set x-amz-acl to public-read by default
+    .pipe(publisher.publish(headers))
+
+    // create a cache file to speed up consecutive uploads
+    .pipe(publisher.cache())
+
+     // print upload updates to console
+    .pipe(awspublish.reporter());
+});
+
+
+
+
+
+
+
+var watcher = gulp.watch('src/**/*.*', ['build']);
+watcher.on('change', function(event) {
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+});
