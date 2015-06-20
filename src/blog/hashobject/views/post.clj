a981cfad9112f109fc8e69dc062b4cb57596bdc7
Anton Podviaznikov <podviaznikov@gmail.com>
2015-06-20 16:25:48 -0700

    change format of metadata keys

--- src/blog/hashobject/views/post.clj
+++ src/blog/hashobject/views/post.clj
@@ -11,20 +11,20 @@
       [:meta {:charset "utf-8"}]
       [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
       [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
-      [:meta {:itemprop "author" :name "author" :content (str (:author metadata) " (" (:author_email metadata) ")" )}]
+      [:meta {:itemprop "author" :name "author" :content (str (:author metadata) " (" (:author-email metadata) ")" )}]
       [:meta {:name "keywords" :itemprop "keywords" :content (:keywords metadata)}]
       [:meta {:name "description" :itemprop "description" :content (:description metadata)}]
-      [:meta {:itemprop "inLanguage" :content (:in_language metadata)}]
-      [:meta {:itemprop "dateCreated" :content (:date_created metadata)}]
-      [:meta {:itemprop "dateModified" :content (:date_modified metadata)}]
-      [:meta {:itemprop "datePublished" :content (:date_published metadata)}]
+      [:meta {:itemprop "inLanguage" :content (:in-language metadata)}]
+      [:meta {:itemprop "dateCreated" :content (:date-created metadata)}]
+      [:meta {:itemprop "dateModified" :content (:date-modified metadata)}]
+      [:meta {:itemprop "datePublished" :content (:date-published metadata)}]
       [:title {:itemprop "name"} (:name metadata)]
       [:link {:rel "shortcut icon" :href "/favicon.ico"}]
       [:link {:rel "publisher" :href "https://plus.google.com/118068495795214676039"}]
       [:link {:rel "author" :href "humans.txt"}]
       [:link {:rel "alternate" :type "application/rss+xml" :title "RSS" :href "feed.rss"}]
-      [:link {:rel "discussionUrl" :href (:discussion_url metadata)}]
-      [:link {:rel "canonical" :href (:canonical_url metadata)}]
+      [:link {:rel "discussionUrl" :href (:discussion-url metadata)}]
+      [:link {:rel "canonical" :href (:canonical-url metadata)}]
       (include-css "/css/app.css")
       (include-css "https://fonts.googleapis.com/css?family=PT+Sans")
       (common/ga)]
@@ -35,17 +35,17 @@
            [:h1 {:itemprop "name"} (:name metadata)]
            (str (:content metadata))
            [:aside.post-meta.small-12.medium-12.columns
-            [:img.author-avatar {:src (:author_avatar metadata) :title (:author metadata)}]
+            [:img.author-avatar {:src (:author-avatar metadata) :title (:author metadata)}]
             [:div.meta-info
               [:div
                 [:span.meta-label "Written by: "]
-                [:a.author-name {:href (:author_url metadata)} (:author metadata)]]
+                [:a.author-name {:href (:author-url metadata)} (:author metadata)]]
               [:div
                 [:span.meta-label "Published: "]
-                [:span (dates/reformat-date (:date_published metadata) "MMM dd, YYYY")]]
+                [:span (dates/reformat-date (:date-published metadata) "MMM dd, YYYY")]]
               [:div
                 [:span.meta-label "Modified: "]
-                [:span (dates/reformat-date (:date_modified metadata) "MMM dd, YYYY")]]
+                [:span (dates/reformat-date (:date-modified metadata) "MMM dd, YYYY")]]
               [:div
                 [:span.meta-label "Published in: "]
                 [:span (:location metadata)]]
@@ -53,5 +53,5 @@
                 [:span.meta-label "Reading time: "]
                 [:span (str (:ttr metadata) " mins")]]]]]
           [:footer.post-discussion.small-12.columns  "Discussion for this post available on "
-            [:a {:href (:discussion_url metadata)} "GitHub"] "."]]
+            [:a {:href (:discussion-url metadata)} "GitHub"] "."]]
          (common/footer)]))
